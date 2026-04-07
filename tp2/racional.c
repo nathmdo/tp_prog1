c481978f4307dad9c92eb5f339ace72c461911a1
Nathalia Marcondes <nath.mdo@gmail.com>
2026-04-07 12:51:40 -0300

    add coisas

--- tp2/racional.c
+++ tp2/racional.c
@@ -10,6 +10,8 @@
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
 
 /*
  * Implemente aqui as funcoes definidas no racionais.h; caso precise,
@@ -20,21 +22,35 @@
 /* retorna um número aleatório entre min e max, inclusive. */
 long aleat (long min, long max)
 {
-  /* implemente aqui */
+  if (min > max)
+  { // Garante que min <= max
+    long temp = min;
+    min = max;
+    max = temp;
+  }
+
+  return min + rand() % (max - min + 1);
 }
 
 /* Máximo Divisor Comum entre a e b      */
-/* calcula o MDC pelo método de Euclides */
 long mdc (long a, long b)
 {
-  /* implemente aqui */
+  long resto;
+
+  while (b != 0)
+  {
+    resto = a % b;
+    a = b;
+    b = resto;
+  }
+
+  return a;
 }
 
 /* Mínimo Múltiplo Comum entre a e b */
-/* mmc = (a * b) / mdc (a, b)        */
 long mmc (long a, long b)
 {
-  /* implemente aqui */
+  return (a * b) / mdc(a, b);
 }
 
 /* Recebe um número racional e o simplifica.
@@ -44,7 +60,111 @@ long mmc (long a, long b)
  * Se r for inválido, devolve-o sem simplificar. */
 struct racional simplifica_r (struct racional r)
 {
-  /* implemente aqui */
+  if (r.den == 0)
+  {
+    return r;
+  }
+
+  // Calcula o MDC do numerador e denominador
+  long divisor = mdc(labs(r.num), labs(r.den));
+
+  r.num = r.num / divisor;
+  r.den = r.den / divisor;
+
+  // jogo de sinais
+  if (r.den < 0)
+  {
+    r.num = -r.num;
+    r.den = -r.den;
+  }
+  return r;
 }
 
 /* implemente as demais funções de racional.h aqui */
+
+
+/* Retorna o numerador do racional r */
+long numerador_r (struct racional r)
+{
+  return r.num;
+}
+
+/* Retorna o denominador do racional r */
+long denominador_r (struct racional r)
+{
+  return r.den;
+}
+
+/* Cria um número racional com o numerador e denominador indicados. */
+struct racional cria_r (long numerador, long denominador)
+{
+  struct racional r;
+  r.num = numerador;
+  r.den = denominador;
+  return r;
+}
+
+/* Retorna 1 se o racional r for válido ou 0 se for inválido.
+ * Um racional é inválido se seu denominador for zero */
+int valido_r (struct racional r)
+{
+  if (r.den == 0)
+  {
+    return 0; // inválido
+  }
+  return 1; // válido
+}
+
+/* Retorna um número racional aleatório na forma simplificada.
+ * Deve ser sorteado o numerador e depois o denominador.
+ * o racional gerado pode ser válido ou inválido.
+ * O numerador e o denominador devem ser inteiros entre min e max. */
+struct racional sorteia_r (long min, long max)
+{
+  long num = aleat(min, max); // sorteia numerador
+  long den = aleat(min, max); // sorteia denominador
+
+  // cria racional com os valores sorteados
+  struct racional r = cria_r(num, den);
+
+  // simplifica e retorna
+  return simplifica_r(r);
+}
+
+
+
+/* Imprime um racional r, respeitando estas regras:
+   - o racional deve ser impresso na forma simplificada;
+   - não imprima espaços em branco e não mude de linha;
+   - a saída deve ter o formato "num/den", a menos dos casos abaixo:
+     - se o racional for inválido, imprime a mensagem "NaN" (Not a Number);
+     - se o numerador for 0, imprime somente "0";
+     - se o denominador for 1, imprime somente o numerador;
+     - se o numerador e denominador forem iguais, imprime somente "1";
+     - se o racional for negativo, o sinal "-" vem antes do numerador;
+     - se numerador e denominador forem negativos, o racional é positivo. */
+void imprime_r (struct racional r);
+
+/* Compara dois racionais r1 e r2. Retorno: -2 se r1 ou r2 for inválido,
+ * -1 se r1 < r2, 0 se r1 = r2 ou 1 se r1 > r2 */
+int compara_r (struct racional r1, struct racional r2);
+
+/* Retorna a soma dos racionais r1 e r2 no parametro *r3.
+ * Retorna 1 se a operacao foi bem sucedida ou
+ *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
+int soma_r (struct racional r1, struct racional r2, struct racional *r3);
+
+/* Retorna a subtracao dos racionais r1 e r2 no parametro *r3.
+ * Retorna 1 se a operacao foi bem sucedida ou
+ *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
+int subtrai_r (struct racional r1, struct racional r2, struct racional *r3);
+
+/* Retorna a multiplicacao dos racionais r1 e r2 no parametro *r3.
+ * Retorna 1 se a operacao foi bem sucedida ou
+ *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
+int multiplica_r (struct racional r1, struct racional r2, struct racional *r3);
+
+/* Retorna a divisao dos racionais r1 e r2 no parametro *r3.
+ * Retorna 1 se a operacao foi bem sucedida ou
+ *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
+int divide_r (struct racional r1, struct racional r2, struct racional *r3);
--- tp2/tp2.c
+++ tp2/tp2.c
@@ -6,13 +6,38 @@
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include "racional.h"
+
+#define MAX 100
+//o racional.c não precisa declarar...
 
 /* coloque aqui as funções auxiliares que precisar neste arquivo */
+
 
 /* programa principal */
 int main ()
 {
-  /* a completar! */
+  //vetor de racionais
+  struct racional vetor[MAX];
+
+  int n;
+
+  //ler n
+  scanf("%d", &n);
+  
+  //se n fora dos parâmetros
+  if(n<=0 || n>=MAX)
+  {
+    return 0;
+  }
+  // if(n <= 0 || n >= MAX) return 0;
+
+  for(int i=0; i<n; i++)
+  {
+    scanf("%ld", num);
+    scanf("%ld", den);
+    vetor[i] = cria_r(num, den);
+  }
 
   return (0) ;
 }
