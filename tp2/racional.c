5be5723efd6f45051c6958c736769543219cc541
Nathalia Marcondes <nath.mdo@gmail.com>
2026-04-09 10:49:14 -0300

    Lógica pronta

--- tp2/racional.c
+++ tp2/racional.c
@@ -32,7 +32,7 @@ long aleat (long min, long max)
   return min + rand() % (max - min + 1);
 }
 
-/* Máximo Divisor Comum entre a e b      */
+/* Máximo Divisor Comum entre a e b */
 long mdc (long a, long b)
 {
   long resto;
@@ -143,28 +143,136 @@ struct racional sorteia_r (long min, long max)
      - se o numerador e denominador forem iguais, imprime somente "1";
      - se o racional for negativo, o sinal "-" vem antes do numerador;
      - se numerador e denominador forem negativos, o racional é positivo. */
-void imprime_r (struct racional r);
+void imprime_r (struct racional r)
+{
 
+  // Racional invalido
+  if (!valido_r(r))
+  {
+    printf("NaN");
+    return;
+  }
+
+  r = simplifica_r(r);
+
+  // Se numerador = 0
+  if (r.num == 0)
+  {
+    printf("0");
+    return;
+  }
+
+  // Se denominador = 1
+  if (r.den == 1)
+  {
+    printf("%ld", r.num);
+    return;
+  }
+
+  if (r.num == r.den)
+  {
+    printf("1");
+    return;
+  }
+
+  printf("%ld/%ld", r.num, r.den);
+
+
+  /*
+  if(!valido_r(r)){
+    printf("NaN");
+  } 
+  
+  r = simplifica_r(r);
+
+  if (r.num == 0){ //se num = 0
+    printf("0");
+  } else if (r.den == 1) //se den = 1
+  {
+    printf("%ld", r.num);
+  } else if (r.num == r.den)// se iguais
+  {
+    printf("1");
+  } else {
+    printf("%ld/%ld", r.num, r.den);
+  }*/
+  
+  
+}
+
+/********************************************************************** */
 /* Compara dois racionais r1 e r2. Retorno: -2 se r1 ou r2 for inválido,
  * -1 se r1 < r2, 0 se r1 = r2 ou 1 se r1 > r2 */
-int compara_r (struct racional r1, struct racional r2);
+int compara_r (struct racional r1, struct racional r2)
+{
+  if(!valido_r(r1) || !valido_r(r2)) return -2;
+  
+  r1 = simplifica_r(r1);
+  r2 = simplifica_r(r2);
+  
+  /* Compara usando produto cruzado
+  Se os numeradores e denominadores forem grandes, a multiplicação pode ultrapassar o limite de int. Usando long long, você reduz o risco de overflow.
+  𝑛𝑢𝑚1⋅𝑑𝑒𝑛2 e 𝑛𝑢𝑚2⋅𝑑𝑒𝑛1  
+  Se 𝑛𝑢𝑚1⋅𝑑𝑒𝑛2<𝑛𝑢𝑚2⋅𝑑𝑒𝑛1,então 𝑟1<𝑟2.
+  Se 𝑛𝑢𝑚1𝑑𝑒𝑛2>𝑛𝑢𝑚2⋅𝑑𝑒𝑛1, então 𝑟1>𝑟2.
+  Se forem iguais, 𝑟1=𝑟2.*/
+  long long a = (long long) r1.num * r2.den;
+  long long b = (long long) r2.num * r1.den;
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+  
+}
 
 /* Retorna a soma dos racionais r1 e r2 no parametro *r3.
  * Retorna 1 se a operacao foi bem sucedida ou
  *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
-int soma_r (struct racional r1, struct racional r2, struct racional *r3);
+int soma_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+  if(!r3 || !valido_r(r1) || !valido_r(r2)) return 0;
+
+  // Calcula soma
+  r3->num = r1.num * r2.den + r2.num * r1.den;
+  r3->den = r1.den * r2.den;
+  *r3 = simplifica_r(*r3);
+  return valido_r(*r3);
+
+
+}
 
 /* Retorna a subtracao dos racionais r1 e r2 no parametro *r3.
  * Retorna 1 se a operacao foi bem sucedida ou
  *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
-int subtrai_r (struct racional r1, struct racional r2, struct racional *r3);
+int subtrai_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+  //se o ponteiro r3 for nulo ou se r1/r2 forem inválidos, não dá pra somar
+  if (!r3 || !valido_r(r1) || !valido_r(r2)) return 0;
+    r3->num = r1.num * r2.den - r2.num * r1.den;
+    r3->den = r1.den * r2.den;
+    *r3 = simplifica_r(*r3); // Simplifica o resultado (por exemplo, 2/4 vira 1/2)
+    return valido_r(*r3); // Retorna 1 se o resultado for válido, 0 se não for
+}
 
 /* Retorna a multiplicacao dos racionais r1 e r2 no parametro *r3.
  * Retorna 1 se a operacao foi bem sucedida ou
  *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
-int multiplica_r (struct racional r1, struct racional r2, struct racional *r3);
-
+int multiplica_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+  if (!r3 || !valido_r(r1) || !valido_r(r2)) return 0;
+    r3->num = r1.num * r2.num;
+    r3->den = r1.den * r2.den;
+    *r3 = simplifica_r(*r3);
+    return valido_r(*r3);
+}
 /* Retorna a divisao dos racionais r1 e r2 no parametro *r3.
  * Retorna 1 se a operacao foi bem sucedida ou
  *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
-int divide_r (struct racional r1, struct racional r2, struct racional *r3);
+int divide_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+   if (!r3 || !valido_r(r1) || !valido_r(r2)) return 0;
+    if (r2.num == 0) return 0; // divisão inválida
+    r3->num = r1.num * r2.den;
+    r3->den = r1.den * r2.num;
+    *r3 = simplifica_r(*r3);
+    return valido_r(*r3);
+}
--- tp2/tp2.c
+++ tp2/tp2.c
@@ -11,7 +11,54 @@
 #define MAX 100
 //o racional.c não precisa declarar...
 
+
 /* coloque aqui as funções auxiliares que precisar neste arquivo */
+
+//Remove os elementos inválidas do vetor e retorna o novo tamanho.
+int remove_invalidos(struct racional v[], int n){
+  int novo_n = 0;
+  for(int i=0; i<n; i++){
+    if(valido_r(v[i])){
+      v[novo_n] = v[i];
+      novo_n++;
+    }
+  }
+  return novo_n;
+}
+
+//Ordena o vetor de racionais usando o Selection Sort
+void ordena_vetor(struct racional v[], int n) 
+{
+  // v é um ponteiro para vetor[0]
+  for(int i=0; i < n - 1; i++){
+    int min = i; //assume que o menor está na pos i
+    for(int j = i + 1; j < n; j++){
+      if(compara_r(v[j], v[min]) < 0){ //compara retorna -1 se r1 for menor
+        min = j; //achou um menor, atualiza o índice
+      }
+    }
+    //troca v[i] com v[min], se necessário
+    if(min != i){
+      struct racional tmp = v[i];
+      v[i] = v[min];
+      v[min] = tmp;
+    }
+  }
+}
+
+//Soma todos os elementos do vetor de racionais
+struct racional soma_vetor(struct racional v[], int n)
+{
+  struct racional soma = cria_r(0,1); //cria um racional "soma" com valor 0/1 (significa zero mas em racional)
+  for(int i=0; i<n; i++){ 
+    struct racional tmp; //variável temporária para guardar o resultado da soma
+    if(soma_r(soma, v[i], &tmp)){
+      soma = tmp; //atualiza 'soma' com o resultado armazenado em tmp
+    }
+  }
+
+  return soma;
+}
 
 
 /* programa principal */
@@ -26,18 +73,47 @@ int main ()
   scanf("%d", &n);
   
   //se n fora dos parâmetros
-  if(n<=0 || n>=MAX)
-  {
-    return 0;
-  }
-  // if(n <= 0 || n >= MAX) return 0;
+  if(n<=0 || n>=MAX) return 0;
 
+  //ler n racionais e por no vetor
   for(int i=0; i<n; i++)
   {
-    scanf("%ld", num);
-    scanf("%ld", den);
+    long num, den;
+    scanf("%ld %ld", &num, &den);
     vetor[i] = cria_r(num, den);
   }
+
+  //Imprime o vetor inicial
+  printf("VETOR = ");
+  for(int i=0; i<n; i++)
+  {
+    imprime_r(vetor[i]);
+    printf(" ");
+  }
+
+  //Imprime o vetor sem elementos inválidos
+  n = remove_invalidos(vetor, n);
+  printf("\nVETOR = ");
+  for(int i=0; i<n; i++){
+    imprime_r(vetor[i]);
+    printf(" ");
+  }
+
+
+  //imprime o vetor ordenado
+  ordena_vetor(vetor, n);
+  printf("\nVETOR = ");
+  for(int i=0; i<n; i++){
+    imprime_r(vetor[i]);
+    printf(" ");
+  }
+
+  //imprime a soma de todos os elementos do vetor
+  struct racional resultado = soma_vetor(vetor, n);
+  printf("\nSOMA = ");
+  imprime_r(resultado);
+
+  printf("\n");  
 
   return (0) ;
 }
