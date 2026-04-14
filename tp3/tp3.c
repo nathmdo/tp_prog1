f42a286e1bcca936deebd01a0c9d2a157d77d969
Nathalia Marcondes <nath.mdo@gmail.com>
2026-04-14 12:40:36 -0300

    avançando no tp3

--- tp3/tp3.c
+++ tp3/tp3.c
@@ -6,6 +6,8 @@
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include "racional.h"
+#define MAX 100
 
 /* coloque aqui as funções auxiliares que precisar neste arquivo */
 
@@ -13,6 +15,30 @@
 int main ()
 {
   /* a completar! */
+
+  int n;
+
+  //ler n
+  scanf("%d", &n);
+  
+  //se n fora dos parâmetros
+  if(n<=0 || n>=MAX) return 0;
+
+
+  /* Inicialmente, você vai alocar dinamicamente um vetor de ponteiros para N números racionais.*/
+  
+
+
+
+  /*Em seguida, você vai inicializar o vetor com ponteiros para N números racionais lidos a partir do teclado e vai inserir estes ponteiros, na mesma ordem da leitura, no vetor.*/
+
+
+
+
+
+
+
+
 
   return 0;
 }
