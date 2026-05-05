7d34e56cfa7b285bf0191ca33717feea9bc01926
Nathalia Marcondes <nath.mdo@gmail.com>
2026-05-05 10:32:56 -0300

    TP4: fazendo as funções criar e destruir

--- tp4/lista.c
+++ tp4/lista.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "lista.h"
-/* coloque demais includes aqui */
 
 /* ------------------- Nao altere estas structs ------------------------- */
 struct nodo {
@@ -18,9 +18,36 @@ struct lista {
 struct lista *lista_cria (){
     struct lista *list = malloc(sizeof(struct lista));
     if(list == NULL) return NULL;
+
+    list->ini = malloc(sizeof(struct nodo)); //inicia o ponteiro do ini
+    if(list->ini == NULL){
+        free(list);
+        return NULL;
+    }
+
+    list->ini->prox = NULL;
+    list->ptr = NULL;
+    list->tamanho = 0;
+
+    return list;
 }
 
-void lista_destroi (struct lista **lista){
+void lista_destroi (struct lista **lista){ //recebe um ponteiro para ponteiro
+    //lista: endereço da variável 
+    //*lista: a lista de verdade
+    //verifica se o ponteiro é válido e se a lista existe
+    if(!lista || !(*lista)) return;
+    
+    struct nodo *atual = (*lista)->ini->prox;
+    while (atual){
+        struct nodo *tmp = atual; //guarda o nodo atual para liberar depois
+        atual = atual->prox; //avança para o próximo
+        free(tmp); //libera o antigo
+    }
+    
+    free((*lista)->ini); // libera o nodo cabeça (ini)
+    free(*lista); //libera a struct em si
+    *lista = NULL; //aterra o ponteiro    
 }
 
 int lista_insere_inicio (struct lista *lista, int chave){
